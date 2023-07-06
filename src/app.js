fcf5013de368ce5f2b5dab9794275ac00583d17f
Alicja <a.wojcikbehar.code@gmail.com>
2023-07-06 11:45:13 +0200

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,82 +1,42 @@
-const inputEmail = document.querySelector(".inputEmail");
-const inputPassword = document.querySelector(".inputPassword");
-const inputCheck = document.querySelector(".inputCheck");
-const btnCreate = document.querySelector(".btn-create");
+const signupButton = document.querySelector(".signup-button");
+const signupForm = document.querySelector(".signup-form");
 
 const errorMessages = {
-  email_incorrect: "No @ in the email",
-  password_incorrect: "The password must be longer than 8 characters long",
-  accept_incorrect: "Consent not confirmed",
+  email: "Email is not correct",
+  password:
+    "The password must be longer than 6 characters long and contain one capital letter and one number",
+  checkbox: "Consent not confirmed",
 };
 
-inputEmail.addEventListener("change", function () {
-  let inputEmailValue = inputEmail.value;
-  if (!inputEmailValue.includes("@")) {
-    showError(errorMessages.email_incorrect);
+function handleInputChange(event) {
+  if (!event.target.validity.valid) {
+    showError(event.target, errorMessages[event.target.type]);
   } else {
-    hideError();
+    hideError(event.target);
   }
-});
-
-inputPassword.addEventListener("change", function () {
-  let inputPasswordValue = inputPassword.value;
-  if (inputPasswordValue.length < 8) {
-    showError(errorMessages.password_incorrect);
-  } else {
-    hideError();
-  }
-});
-
-inputCheck.addEventListener("change", function () {
-  let inputCheckValue = inputCheck.value;
-  if (!inputCheckValue) {
-    showError(errorMessages.accept_incorrect);
-  } else {
-    hideError();
-  }
-});
-
-btnCreate.addEventListener("click", function (event) {
-  event.preventDefault();
-  let inputEmailValue = inputEmail.value;
-  let inputPasswordValue = inputPassword.value;
-  let inputCheckValue = inputCheck.checked;
-
-  let errorMessagesArray = [];
-
-  if (!inputEmailValue.includes("@")) {
-    errorMessagesArray.push(errorMessages.email_incorrect);
-  }
-
-  if (inputPasswordValue.length < 8) {
-    errorMessagesArray.push(errorMessages.password_incorrect);
-  }
-
-  if (!inputCheckValue) {
-    errorMessagesArray.push(errorMessages.accept_incorrect);
-  }
-
-  if (errorMessagesArray.length > 0) {
-    showError(errorMessagesArray);
-    return;
-  } else {
-    alert("Your account was created successfully!");
-  }
-});
-
-function showError(messages) {
-  const errorContent = document.querySelector(".content-error");
-  errorContent.innerHTML = "";
-
-  messages.forEach((message) => {
-    const errorMessage = document.createElement("div");
-    errorMessage.innerHTML = message;
-    errorContent.appendChild(errorMessage);
-  });
-  errorContent.style.display = "block";
+  signupButton.disabled = !signupForm.reportValidity();
 }
 
-function hideError() {
-  const errorElements = document.querySelector(".content-error");
-  errorElements.style.display = "none";
+signupForm.addEventListener("change", handleInputChange);
+
+signupButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (!signupForm.reportValidity()) {
+    return;
+  }
+  alert("Your account was created successfully!");
+});
+
+function showError(inputElement, message) {
+  const errorElement =
+    inputElement.parentElement.querySelector(".error-message");
+  errorElement.textContent = message;
+  errorElement.style.display = "block";
+}
+
+function hideError(inputElement) {
+  const errorElement =
+    inputElement.parentElement.querySelector(".error-message");
+  errorElement.textContent = "";
+  errorElement.style.display = "none";
 }
